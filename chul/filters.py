1f36cbeaea9b39eda61e78449a0da1ca5a75e2f9
ertush <erotush77@gmail.com>
2025-08-07 17:04:21 +0300

    Removed facility_code filter in CommunityHealthUnitFilter

--- chul/filters.py
+++ chul/filters.py
@@ -86,7 +86,7 @@ class CommunityHealthUnitFilter(CommonFieldsFilterset):
         name='facility__ward__constituency__county')
     sub_county = ListCharFilter(
         name='facility__ward__sub_county')
-    facility_code = ListCharFilter(name="facility_code")
+    # facility_code = ListCharFilter(name="facility_code")
 
     is_approved = django_filters.TypedChoiceFilter(
         choices=BOOLEAN_CHOICES, coerce=strtobool
